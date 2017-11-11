e9b6b42cc68e7d65f8265f94b1609fd635484488
Ye Liu <yeliu@YedeMacBook-Pro.local>
2017-11-11 16:35:22 +0800

    add new type and clean up

--- sutra-process/cmd.sh
+++ sutra-process/cmd.sh
@@ -1,5 +1,14 @@
-curl -XPUT -H 'Content-Type:application/json;charset=utf-8' --data-binary @./mapping1.txt http://192.168.16.69:9200/tripitaka
+curl -XPUT -H 'Content-Type:application/json;charset=utf-8' --data-binary @./mappings.txt http://192.168.16.69:9200/tripitaka
 
 for file in `ls data`; do
 curl -XPUT -H 'Content-Type:application/json;charset=utf-8' --data-binary @./data/$file http://192.168.16.69:9200/_bulk;
 done
+
+
+while read line; do 
+curl -XPOST -H 'Content-Type:application/json;charset=utf-8' http://192.168.16.69:9200/tripitaka/suggest -d "
+{
+\"candidate\": \"$line\",
+\"search_count\": 0
+}"; 
+done < terms-with-english.txt
